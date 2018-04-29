7fcf9558474c264be8ede2cf467d936ac8b73a3c
mxie <15623530290@163.com>
2018-04-30 00:01:31 +0800

    cdn 配置

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -41,6 +41,10 @@ module.exports = {
       '@': resolve('src'),
     }
   },
+  externals: {
+    'vue': 'Vue',
+    'element-ui': 'ELEMENT'
+  },
   module: {
     rules: [
       ...(config.dev.useEslint ? [createLintingRule()] : []),
